4ade13dedd112944baa39eede6bea6264c4a61db
Jesse Mykolyn <jrmykolyn@gmail.com>
2016-10-17 22:08:20 -0400

    Moved HEX code-related JS into discrete function. Partially automated 'apply styles' logic.

--- js/main.js
+++ js/main.js
@@ -45,10 +45,8 @@ $( document ).ready( function() {
 
 			// Loop over characters.
 			for ( var i = 0, x = charsArr.length; i < x; i++ ) {
-				// Alias current array member as `currChar`.
+				// Declare local vars.
 				var currChar = charsArr[i];
-
-				// Create new <span> elem. 
 				var wrappedElem = document.createElement( 'span' );
 
 				// IF `currChar` is an empty space, replace with unicode char.
@@ -60,15 +58,6 @@ $( document ).ready( function() {
 				var multiplier = ( Math.random() > 0.5 ) ? 1 : -1,
 					degreesToRotate = ( Math.round( Math.random() * 20 ) * multiplier );
 
-				// Build random HEX code.
-				var validHexChars = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'],
-					hexCodeLength = 6,
-					hexCode = '#00AA';
-
-				for ( var j = 0, y = hexCodeLength - ( hexCode.length - 1 ); j < y; j++ ) {
-					hexCode += validHexChars[Math.floor( Math.random() * validHexChars.length )];
-				}
-
 				// Add current character to <span>.
 				wrappedElem.appendChild( document.createTextNode( currChar ) );
 
@@ -77,19 +66,49 @@ $( document ).ready( function() {
 				wrappedElem.style.float = 'left';
 
 				// Apply decorative styles to <span>.
-				wrappedElem.style.color = settings['color'] || hexCode;
-				wrappedElem.style['background'] = settings['background'];
-				wrappedElem.style['background-color'] = settings['background-color'];
-				wrappedElem.style['box-shadow'] = settings['box-shadow'];
-				wrappedElem.style['text-shadow'] = settings['text-shadow'];
-				wrappedElem.style.padding = settings['padding'];
+				for ( var prop in defaults ) {
+					wrappedElem.style[prop] = settings[prop] || defaults[prop]
+				}
+
+				wrappedElem.style['color'] = settings['color'] || getRandomHex( options.hexSeed || null );
+				wrappedElem.style['background-color'] = settings['background-color'] || getRandomHex( options.hexSeed || null );
 				wrappedElem.style.transform = 'rotate( ' + degreesToRotate +  'deg)';
 
 				// Add <span> tag to matched elem.
 				el.appendChild( wrappedElem );
 			}
 		} );
+
+
+		// --------------------------------------------------
+		// DECLARE FUNCTIONS
+		// --------------------------------------------------
+		function getRandomHex( seed ) {
+			// Define local vars.
+			var validHexChars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'],
+				hexCodeLength = 6,
+				hexCode = '#';
+
+			// Validate `seed` arg. and add chars to `hexCode`.
+			if ( typeof seed === 'string' && seed.length <= 6 ) {
+				for ( var i = 0, x = seed.length; i < x; i++ ) {
+					var currChar = seed.charAt( i );
+
+					if ( validHexChars.indexOf( currChar.toUpperCase() ) !== -1 ) {
+						hexCode += currChar;
+					}
+				}
+			}
+
+			// Fill out remainder of HEX code with random chars from `validHexChars`.
+			for ( var i = 0, x = hexCodeLength - ( hexCode.length - 1 ); i < x; i++ ) {
+				hexCode += validHexChars[Math.floor( Math.random() * validHexChars.length )];
+			}
+
+			return hexCode;
+		}
 	})( {
+			'hexSeed': '',
 			'color': 'white',
 			'background-color': 'blue',
 			'text-shadow': '0px 1px 1px rgba(0,0,0,0.5)',
